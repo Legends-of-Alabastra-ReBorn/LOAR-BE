9ce661cd8640bd618a4b8b0997b8dd35aaa4fc80
Miguel <diaz4674@gmail.com>
2020-03-03 22:44:10 -0800

    test heroku

--- auto/mulligan.py
+++ auto/mulligan.py
@@ -4,7 +4,6 @@ import sys
 filename = 'main.py'
 
 while True:
-    print("STIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIL")
     print(f'\nSTARTING {filename}')
     p = Popen('python3 ' + filename, shell=True)
     p.wait()
